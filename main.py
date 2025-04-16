cabcb276c7d010e4dd8c6934e06b171cab72b2f7
RecursiveDeveloper <johanjesus133@hotmail.com>
2025-04-16 10:54:41 -0500

    Se tradujeron los textos de español a ingles. Se eliminaron espacios innecesarios entre lineas

--- main.py
+++ main.py
@@ -3,23 +3,18 @@ License: Apache
 Organization: UNIR
 """
 
-import os
-import sys
+import os, sys
 
 DEFAULT_FILENAME = "words.txt"
 DEFAULT_DUPLICATES = False
 
-
 def sort_list(items, ascending=True):
     if not isinstance(items, list):
-        raise RuntimeError(f"No puede ordenar {type(items)}")
-
+        raise RuntimeError(f"It can't be sorted {type(items)}")
     return sorted(items, reverse=(not ascending))
-
 
 def remove_duplicates_from_list(items):
     return list(set(items))
-
 
 if __name__ == "__main__":
     filename = DEFAULT_FILENAME
@@ -28,11 +23,11 @@ if __name__ == "__main__":
         filename = sys.argv[1]
         remove_duplicates = sys.argv[2].lower() == "yes"
     else:
-        print("Se debe indicar el fichero como primer argumento")
-        print("El segundo argumento indica si se quieren eliminar duplicados")
+        print("Filename must be declared as the first argument")
+        print("The second argument declares if duplicated words must be removed")
         sys.exit(1)
 
-    print(f"Se leerán las palabras del fichero {filename}")
+    print(f"Words from {filename} will be read")
     file_path = os.path.join(".", filename)
     if os.path.isfile(file_path):
         word_list = []
@@ -40,7 +35,7 @@ if __name__ == "__main__":
             for line in file:
                 word_list.append(line.strip())
     else:
-        print(f"El fichero {filename} no existe")
+        print(f"The file {filename} doesn't exists")
         word_list = ["ravenclaw", "gryffindor", "slytherin", "hufflepuff"]
 
     if remove_duplicates:
