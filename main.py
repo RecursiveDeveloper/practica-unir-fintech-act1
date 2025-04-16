65e049d9221e2dfe9cb7ca47944578eebd94579f
Patrick Tanta <krussdev@gmail.com>
2025-04-16 00:02:53 -0500

    feat(main.py): add parameter to control ordering

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import sys
 
 DEFAULT_FILENAME = "words.txt"
 DEFAULT_DUPLICATES = False
+DEFAULT_ASCENDING = True
 
 
 def sort_list(items, ascending=True):
@@ -24,15 +25,20 @@ def remove_duplicates_from_list(items):
 if __name__ == "__main__":
     filename = DEFAULT_FILENAME
     remove_duplicates = DEFAULT_DUPLICATES
-    if len(sys.argv) == 3:
+    ascending_order = DEFAULT_ASCENDING
+    if len(sys.argv) == 4:
         filename = sys.argv[1]
         remove_duplicates = sys.argv[2].lower() == "yes"
+        ascending_order = sys.argv[3].lower() == "asc"
     else:
         print("Se debe indicar el fichero como primer argumento")
         print("El segundo argumento indica si se quieren eliminar duplicados")
+        print("El tercer argumento indica si se quieren ordenar 'asc' o 'desc' para orden ascendente o descendente")
         sys.exit(1)
 
     print(f"Se leerán las palabras del fichero {filename}")
+    print(f"Orden: {'ascendente' if ascending_order else 'descendente'}")
+
     file_path = os.path.join(".", filename)
     if os.path.isfile(file_path):
         word_list = []
@@ -46,4 +52,4 @@ if __name__ == "__main__":
     if remove_duplicates:
         word_list = remove_duplicates_from_list(word_list)
 
-    print(sort_list(word_list))
+    print(sort_list(word_list, ascending=ascending_order))
